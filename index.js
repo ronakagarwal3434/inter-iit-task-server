42782347106ae9ce17cfa6e2b4f9154453dd27d1
ronakagarwal3434 <60756098+ronakagarwal3434@users.noreply.github.com>
2022-02-20 22:57:18 +0530

    make it better

--- index.js
+++ index.js
@@ -58,4 +58,6 @@ app.get("/videolinks/:page", async (req, res) => {
     }
 });
 
-app.listen(3001, () => console.log("Server is running on port 3001"));
+app.listen(process.env.PORT || 3001, () =>
+    console.log("Server is running on port 3001")
+);
